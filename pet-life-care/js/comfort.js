8f9ddca061fd7282a972d71d81edbdace8467e3b
dongheoni <dongheoni031@gmail.com>
2026-06-09 06:42:38 +0000

    장례 서비스 페이지 완성

--- pet-life-care/js/comfort.js
+++ pet-life-care/js/comfort.js
@@ -1,3 +1,12 @@
-const random =
-messages[Math.floor(Math.random()*messages.length)];
+const messages = [
+    "함께한 시간은 소중한 추억으로 남습니다.",
+    "사랑은 기억 속에서 계속됩니다.",
+    "소중한 가족과의 추억은 사라지지 않습니다.",
+    "반려동물은 언제나 마음속에서 함께합니다."
+];
+
+const randomMessage =
+    messages[Math.floor(Math.random() * messages.length)];
+
+document.getElementById("message").innerText = randomMessage;
 
